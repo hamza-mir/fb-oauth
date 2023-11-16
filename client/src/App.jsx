ee59772ab4d6f5b7e31237ba0911e78985fd8d31
Hamza Mir <hamza.mir66@gmail.com>
2023-11-16 13:18:48 +0500

    2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import FacebookLogin from "@greatsumini/react-facebook-login";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  // const responseFacebook = async (response) => {
+  //   if (response.status === "unknown") {
+  //     window.location.reload(false);
+  //   }
+
+  //   let pageTokenRes = await axios.get(
+  //     `https://graph.facebook.com/${response.userID}/accounts?access_token=${response.accessToken}`
+  //   );
+
+  //   let userPages = pageTokenRes.data.data;
+  //   let savedUser = await axios.post(`${backendUrl}/users`, {
+  //     name: response.name,
+  //     userId: response.userID,
+  //     accessToken: response.accessToken,
+  //     profilepictureurl: response.picture.data.url,
+  //     pages: userPages,
+  //   });
+  //   setFbResponse(savedUser.data);
+
+  //   localStorage.setItem("userData", JSON.stringify(savedUser.data));
+
+  //   let now = new Date().getTime();
+  //   localStorage.setItem("setupTime", now);
+
+  //   let pageTokens = [];
+  //   userPages.forEach((page) => {
+  //     pageTokens.push(page.access_token);
+  //   });
+  // };
 
   return (
     <>
@@ -24,12 +54,24 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
+        <FacebookLogin
+          appId={process.env.FACEBOOK_APP_ID}
+          onSuccess={(response) => {
+            console.log("Login Success!", response);
+          }}
+          onFail={(error) => {
+            console.log("Login Failed!", error);
+          }}
+          onProfileSuccess={(response) => {
+            console.log("Get Profile Success!", response);
+          }}
+        />
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
