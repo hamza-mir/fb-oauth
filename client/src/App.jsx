b6f2524b71e25a5c25832c8ed31592b17c99f088
Hamza Mir <hamza.mir66@gmail.com>
2023-11-16 14:14:02 +0500

    6

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,39 +2,39 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import FacebookLogin from "@greatsumini/react-facebook-login";
-
+import FacebookLogin from "react-facebook-login/dist/facebook-login-render-props";
 function App() {
   const [count, setCount] = useState(0);
-  // const responseFacebook = async (response) => {
-  //   if (response.status === "unknown") {
-  //     window.location.reload(false);
-  //   }
 
-  //   let pageTokenRes = await axios.get(
-  //     `https://graph.facebook.com/${response.userID}/accounts?access_token=${response.accessToken}`
-  //   );
+  const responseFacebook = async (response) => {
+    if (response.status === "unknown") {
+      window.location.reload(false);
+    }
 
-  //   let userPages = pageTokenRes.data.data;
-  //   let savedUser = await axios.post(`${backendUrl}/users`, {
-  //     name: response.name,
-  //     userId: response.userID,
-  //     accessToken: response.accessToken,
-  //     profilepictureurl: response.picture.data.url,
-  //     pages: userPages,
-  //   });
-  //   setFbResponse(savedUser.data);
+    let pageTokenRes = await axios.get(
+      `https://graph.facebook.com/${response.userID}/accounts?access_token=${response.accessToken}`
+    );
 
-  //   localStorage.setItem("userData", JSON.stringify(savedUser.data));
+    let userPages = pageTokenRes.data.data;
+    let savedUser = {
+      name: response.name,
+      userId: response.userID,
+      accessToken: response.accessToken,
+      profilepictureurl: response.picture.data.url,
+      pages: userPages,
+    };
+    // setFbResponse(savedUser.data);
 
-  //   let now = new Date().getTime();
-  //   localStorage.setItem("setupTime", now);
+    localStorage.setItem("userData", JSON.stringify(savedUser));
 
-  //   let pageTokens = [];
-  //   userPages.forEach((page) => {
-  //     pageTokens.push(page.access_token);
-  //   });
-  // };
+    let now = new Date().getTime();
+    localStorage.setItem("setupTime", now);
+
+    let pageTokens = [];
+    userPages.forEach((page) => {
+      pageTokens.push(page.access_token);
+    });
+  };
 
   return (
     <>
@@ -58,15 +58,7 @@ function App() {
           appId={1542339499918746}
           scope="pages_show_list,pages_messaging"
           fields="name,email,picture"
-          onSuccess={(response) => {
-            console.log("Login Success!", response);
-          }}
-          onFail={(error) => {
-            console.log("Login Failed!", error);
-          }}
-          onProfileSuccess={(response) => {
-            console.log("Get Profile Success!", response);
-          }}
+          callback={responseFacebook}
         />
       </div>
       <p className="read-the-docs">
